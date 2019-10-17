8e737c1cd2cf6132e64bd5dc76421a1e3e86feb8
tbrabant3 <35499106+tbrabant3@users.noreply.github.com>
2019-10-17 13:03:36 -0400

    Added algorithm Files

---
+++ trees.py
@@ -0,0 +1,12 @@
+# Author: Tyler Brabant & Ashton Allen
+# Class:  CSI-270-01
+# Certification of Authenticity:
+# I certify that this is entirely my own work, except where I have given fully documented
+# references to the work of others.  I understand the definition and consequences of
+# plagiarism and acknowledge that the assessor of this assignment may, for the purpose of
+# assessing this assignment reproduce this assignment and provide a copy to another member
+# of academic staff and / or communicate a copy of this assignment to a plagiarism checking
+# service(which may then retain a copy of this assignment on its database for the purpose
+# of future plagiarism checking).
+
+
