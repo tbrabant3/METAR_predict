38cb05d11a617ba6403824f4094a405d82d54a80
tbrabant3 <35499106+tbrabant3@users.noreply.github.com>
2019-10-22 12:30:38 -0400

    knn and data trees

--- knn.py
+++ knn.py
@@ -9,3 +9,51 @@
 # service(which may then retain a copy of this assignment on its database for the purpose
 # of future plagiarism checking).
 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.neighbors import KNeighborsClassifier
+
+
+def load_dataset(file):
+    dataset = pd.read_csv(file, delimiter=',')
+    X = dataset.iloc[:, :-1]
+    y = dataset.iloc[:, -1]
+
+    # Transform any fields into their correct data type
+
+    # Drop specific columns that don't matter
+    return X, y
+
+
+def encode(X):
+    # Encode the data
+    label_encoder = preprocessing.LabelEncoder()
+    for col in X.columns:
+        X[col] = label_encoder.fit_transform(X[col])
+
+
+def standardScale(test, train):
+    # Scale / Normalize data
+    scaler = preprocessing.StandardScaler(with_mean=True, with_std=True)
+    train = scaler.fit_transform(train)
+    test = scaler.fit_transform(test)
+    return train, test
+
+def main():
+    X, y = load_dataset("ENTER DATASET PATH HERE")
+    encode(X)
+
+    # Split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=30)
+    X_train, X_test = standardScale(X_test, X_train)
+
+    # Classifier to setup amount of neighbors and weighting type
+    classifier = KNeighborsClassifier(n_neighbors=3, weights="distance", metric="minkowski")
+    classifier.fit(X_train, y_train)
+
+    print(classifier.score(X_test, y_test))
+
+
+if __name__ == '__main__':
+    main()
--- trees.py
+++ trees.py
@@ -9,4 +9,51 @@
 # service(which may then retain a copy of this assignment on its database for the purpose
 # of future plagiarism checking).
 
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.tree import DecisionTreeClassifier
+
+# Change this number
+SEED = 1
+
+
+def load_dataset(file):
+    dataset = pd.read_csv(file, delimiter=',')
+    X = dataset.iloc[:, :-1]
+    y = dataset.iloc[:, -1]
+
+    # Transform any fields into their correct data type
+
+    # Drop specific columns that don't matter
+    return X, y
+
+
+def encode(X):
+    # Encode the data
+    label_encoder = preprocessing.LabelEncoder()
+    for col in X.columns:
+        X[col] = label_encoder.fit_transform(X[col])
+
+
+def standard_scale(test, train):
+    # Scale / Normalize data
+    scaler = preprocessing.StandardScaler(with_mean=True, with_std=True)
+    train = scaler.fit_transform(train)
+    test = scaler.fit_transform(test)
+    return train, test
+
+
+def main():
+    X, y = load_dataset("ENTER DATASET PATH HERE")
+    encode(X)
+
+    # Split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=30)
+    X_train, X_test = standard_scale(X_test, X_train)
+
+    dt = DecisionTreeClassifier(max_depth=6, random_state=SEED)
+    # What kind of kind of tree?
+    # Recreation
+
 
