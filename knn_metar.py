b824c750ae4c65d096cd554dff966ea4d5e144a8
ashtonwallen <aashton482@gmail.com>
2019-10-23 16:07:09 -0400

    Comments and pep8

--- knn_metar.py
+++ knn_metar.py
@@ -1,10 +1,10 @@
 # Author: Ashton Allen
-#  Class:  CSI-330-01
+#  Class:  CSI-480-01
 #  Certification of Authenticity:
 # I certify that this is entirely my own work, except where I have given fully documented
 # references to the work of others.  I understand the definition and consequences of
 # plagiarism and acknowledge that the assessor of this assignment may, for the purpose of
-# assessing this assignment reproduce this assignment and provide a copy to anothermember
+# assessing this assignment reproduce this assignment and provide a copy to another member
 # of academic staff and / or communicate a copy of this assignment to a plagiarism checking
 # service(which may then retain a copy of this assignment on its database for the purpose
 # of future plagiarism checking).
@@ -14,19 +14,23 @@ from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.neighbors import KNeighborsClassifier
 
+
 def load_dataset(csv):
+    # load in csv
     dataset = pd.read_csv(csv, delimiter=',')
 
+    # drop not important columns
     df = dataset.drop(columns=['station', 'date', 'time', 'ORIGIN', 'DEST'])
 
+    # encode non numeric column
     label_encoder = preprocessing.LabelEncoder()
     df['skyc1'] = label_encoder.fit_transform(df['skyc1'])
 
-
+    # set up targets
     X = df.iloc[:, :-1]
     y = df.iloc[:, -1]
 
-    return X,y
+    return X, y
 
 
 def scale_data(testing, training):
@@ -40,13 +44,15 @@ def scale_data(testing, training):
 
 def main():
     accuracy = []
+
     X, y = load_dataset('jfk_metars.csv')
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=30)
     X_test, X_train  = scale_data(X_test, X_train)
 
-    # Classifier to setup amount of neighbors and weighting type
     classifier = KNeighborsClassifier(n_neighbors=2, weights="uniform")
     classifier.fit(X_train, y_train)
+
     print(X_train)
 
     accuracy.append(classifier.score(X_test, y_test))
