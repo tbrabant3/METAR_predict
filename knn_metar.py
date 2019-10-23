e634b95f7f916681fb44cd33e464ae1a0e57bed7
ashtonwallen <aashton482@gmail.com>
2019-10-23 14:05:18 -0400

    KNN set up

---
+++ knn_metar.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.neighbors import KNeighborsClassifier
+
+
+dataset = pd.read_csv("jfk_metars.csv", delimiter=',')
+
+df = dataset.drop(columns=['station','date','Time2','ORIGIN','DEST', 'date2','Time'])
+
+label_encoder = preprocessing.LabelEncoder()
+df['skyc1'] = label_encoder.fit_transform(df['skyc1'])
+
+X = df.iloc[:, :-1]
+y = df.iloc[:, -1]
+
+# Split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20)
+
+# Scale / Normalize data
+scaler = preprocessing.StandardScaler(with_mean=True, with_std=True)
+
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.fit_transform(X_test)
+
+test = []
+
+# Classifier to setup amount of neighbors and weighting type
+classifier = KNeighborsClassifier(n_neighbors=3, weights="uniform")
+classifier.fit(X_train, y_train)
+
+test.append(classifier.score(X_test, y_test))
+
+# Use the training data to train the data then predict
+print(test)
+
+
+
+
